b8348eac5703bd04311a9f71ae133e65cc747c54
m.ali petek <malipetek@gmail.com>
2023-06-21 13:31:31 +0300

    add gtag again layout not working

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,23 @@
 import { PolarisProvider } from '../components/PolarisProvider'
- 
+import Script from 'next/script';
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <PolarisProvider>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-D1W9X7R7F0"
+        strategy="afterInteractive"
+      />
+      <Script
+          id="gtag-init"
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-D1W9X7R7F0');`,
+          }}
+        />
       <Component {...pageProps} />
     </PolarisProvider>
   )
